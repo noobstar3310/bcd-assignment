7c740d8107fc9e24c9fbb02458dde65524e9f057
eggwae <salmonfish3310@gmail.com>
2025-02-10 14:36:25 +0800

    Import

--- src/TrackingNFT.sol
+++ src/TrackingNFT.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "./SupplyChainAccess.sol";
 
 contract TrackingNFT is ERC721, ERC721URIStorage {
